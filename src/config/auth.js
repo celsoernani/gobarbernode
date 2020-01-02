e38930ffdb62ff42bb685946dd0675e195afbd41
celsoernani <celsoernane@gmail.com>
2020-01-02 19:51:12 -0300

    auth and create user

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "d58dfdc9ae1892e3b0e4a1cb0b8812d5",
+  expiresIn: "7d"
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "Celso6010",
+  database: "gobarber",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
