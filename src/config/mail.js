a5806c79f1d71056a743ef4431c7dbbbea3cc45b
celsoernani <celsoernane@gmail.com>
2020-01-17 20:39:05 -0300

    send email to provider

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '99b257f38f8d22',
+    pass: '7038574a856398',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
